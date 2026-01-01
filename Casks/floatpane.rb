9444c3007db3e95e01c8f1594d0246933eef605f
Drew Smirnoff <me@andrinoff.com>
2026-01-01 23:19:29 +0400

    Update floatpane.rb for architecture and homepage

--- Casks/floatpane.rb
+++ Casks/floatpane.rb
@@ -1,6 +1,7 @@
 cask "floatpane" do
   version "1.5.0"
-
+  arch arm: "arm64", intel: "x64"
+  
   on_arm do
     sha256 "602d826aae6fe0aa0b3f30d4d0eec2f5f203f3c5f7ed38a155eddd4a34b61c1c"
     url "https://github.com/floatpane/floatpane/releases/download/v#{version}/floatpane-#{version}-arm64.dmg"
@@ -12,9 +13,9 @@ cask "floatpane" do
 
   name "floatpane"
   desc "A wallpaper manager app for macOS"
-  homepage "https://github.com/floatpane/floatpane"
+  homepage "https://floatpane.com"
 
-  app "floatpane.app"
+  app "floatpane-#{arch}.app", target: "floatpane.app"
 
   # This section is optional but recommended for a clean uninstall
   zap trash: [
